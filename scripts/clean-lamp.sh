5fba8e5f83229fa98ebcc7a7a512d2aff21322c7
goffinet <goffinet@goffinet.org>
2018-11-21 18:26:49 +0100

    Update clean-lamp.sh

--- scripts/clean-lamp.sh
+++ scripts/clean-lamp.sh
@@ -6,13 +6,13 @@ systemctl disable httpd mariadb php-fpm
 systemctl disable apache2 mysql
 dnf -y remove mariadb mariadb-server httpd php-fpm
 apt-get -y --purge remove apache2
-apt-get remove --purge mysql-server mysql-client mysql-common mariadb-server
+apt-get -y --purge remove mysql-server mysql-client mysql-common mariadb-server
 apt-get autoclean
 rm -rf /var/lib/mysql/
 rm -rf /etc/mysql/
-rm -r /etc/my.cnf
-rm -r ~/.my.cnf
-rm -rf /var/www/html/*
+rm -f /etc/my.cnf
+rm -f ~/.my.cnf
+rm -rf /var/www/html
 rm -rf /etc/httpd*
 rm -rf /etc/apache2/*
 rm -rf /etc/php-fpm*
